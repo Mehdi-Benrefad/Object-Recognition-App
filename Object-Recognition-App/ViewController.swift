e8e4980372b135d989764e5542259888af07de12
Mehdi Benrefad <benrefadmehdi@gmail.com>
2021-06-05 04:50:53 +0100

    get image from galery

--- Object-Recognition-App/ViewController.swift
+++ Object-Recognition-App/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UIImagePickerControllerDelegate , UINavigationControllerDelegate {
+    @IBOutlet weak var imageSelect: UIImageView!
+    @IBOutlet weak var shoeLabel: UILabel!
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //definir le viewController en tant que delegate de l'image picker
+        imagePicker.delegate = self
+        //definir la source de l'image
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+        
     }
 
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        //on verifie si l'utilisateur a selectionne une image correctement
+         if let userPickerImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                    imageSelect.image = userPickerImage
+                    guard let ciimage = CIImage(image: userPickerImage) else {
+                        fatalError("Failed to convert UI image to ciimage")
+                    }
+        }
+         imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func cameraBTN(_ sender: UIBarButtonItem) {
+        present(imagePicker,animated: true,completion: nil)
+    }
+    
 }
+
 
